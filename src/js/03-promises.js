e175c090ccd5b16541242d99df2895561d499a81
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-06-04 16:02:34 +0100

    Added promise - task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmitForm);
+
+let timeIntervalId = null;
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = event.target;
+
+  setTimeout(() => {
+    let position = 0;
+
+    timeIntervalId = setInterval(() => {
+      if (amount.value <= position) {
+        clearInterval(timeIntervalId);
+      } else {
+        const localDelay = position * Number(step.value) + Number(delay.value);
+        position += 1;
+
+        createPromise(position, localDelay)
+          .then(({ position, delay }) => {
+            console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          })
+          .catch(({ position, delay }) => {
+            console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+          });
+      }
+    }, step.value);
+  }, delay.value);
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
 }
